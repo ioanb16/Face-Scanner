be8cb4c25fe6a40595bb40f70257e1fdca216a52
ioan <barberioan6@gmail.com>
2025-11-13 20:27:30 +0000

    imported cv2 , created HC classifier and accessed webcam

--- FS.py
+++ FS.py
@@ -1,1 +1,16 @@
-h
+import cv2
+
+
+""" Haar Cascade Classifier for Face Detection """
+
+face_classifier = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+
+""" Capture Video from Webcam """
+
+video_capture = cv2.VideoCapture(0)
+ret, frame = video_capture.read()
+
+"""create a face detection function"""
+
+
